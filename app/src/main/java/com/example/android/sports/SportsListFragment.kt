4d9f34fb2c8be947a9bcbf7f4df5f9a08fa211b0
Yuga Ganesh Vasaikar <yugav2001@gmail.com>
2023-06-10 17:17:40 +0530

    handling onbackpress event for small devices

--- app/src/main/java/com/example/android/sports/SportsListFragment.kt
+++ app/src/main/java/com/example/android/sports/SportsListFragment.kt
@@ -20,9 +20,11 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.OnBackPressedCallback
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.navigation.fragment.findNavController
+import androidx.slidingpanelayout.widget.SlidingPaneLayout
 import com.example.android.sports.databinding.FragmentSportsListBinding
 
 /**
@@ -44,15 +46,41 @@ class SportsListFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         val binding = FragmentSportsListBinding.bind(view)
 
+        val slidingPane = binding.slidingPaneLayout
         // Initialize the adapter and set it to the RecyclerView.
         val adapter = SportsAdapter {
             // Update the user selected sport as the current sport in the shared viewmodel
             // This will automatically update the dual pane content
             sportsViewModel.updateCurrentSport(it)
             // Navigate to the details screen
-            binding.slidingPaneLayout.openPane()
+            slidingPane.openPane()
         }
         binding.recyclerView.adapter = adapter
         adapter.submitList(sportsViewModel.sportsData)
+        requireActivity().onBackPressedDispatcher.addCallback(viewLifecycleOwner,SportsListOnBackPressedCallBack(slidingPane))
+
     }
+}
+
+class SportsListOnBackPressedCallBack(private val slidingPaneLayout: SlidingPaneLayout) : OnBackPressedCallback(slidingPaneLayout.isSlideable && slidingPaneLayout.isOpen),
+SlidingPaneLayout.PanelSlideListener{
+    init {
+        slidingPaneLayout.addPanelSlideListener(this)
+    }
+    override fun handleOnBackPressed() {
+        slidingPaneLayout.closePane()
+    }
+
+    override fun onPanelSlide(panel: View, slideOffset: Float) {
+    }
+
+    override fun onPanelOpened(panel: View) {
+        isEnabled=true
+    }
+
+    override fun onPanelClosed(panel: View) {
+        isEnabled=false
+    }
+
+
 }
